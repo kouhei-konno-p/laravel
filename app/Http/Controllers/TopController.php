6fd8e58b834f43777d4728f96b2cc3c777a83dcd
mintae <2mintae@gmail.com>
2021-06-17 09:56:38 +0900

    create top controller

---
+++ app/Http/Controllers/TopController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class TopController extends Controller
+{
+    
+
+    public function show()
+    {
+        return view('top', ['title' => 'Cheata']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TopController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TopController::class, 'show']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
